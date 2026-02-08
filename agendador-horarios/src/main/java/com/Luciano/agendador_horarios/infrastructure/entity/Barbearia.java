9e71b5662616ff7848d42dd0f4772b87a20b124b
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-02-07 22:16:56 -0300

    Implementando as entidades do projeto

---
+++ agendador-horarios/src/main/java/com/Luciano/agendador_horarios/infrastructure/entity/Barbearia.java
@@ -0,0 +1,4 @@
+package com.Luciano.agendador_horarios.infrastructure.entity;
+
+public class Barbearia {
+}
---
+++ agendador-horarios/src/main/java/com/Luciano/agendador_horarios/infrastructure/repository/BarbeariaRepository.java
@@ -0,0 +1,4 @@
+package com.Luciano.agendador_horarios.infrastructure.repository;
+
+public interface BarbeariaRepository {
+}
