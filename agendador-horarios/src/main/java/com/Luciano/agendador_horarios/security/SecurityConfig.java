c271a5129d3656004a3d73ebcfac4c573126f87b
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-18 19:41:12 -0300

    Implementando nova logica do (Spring Security) para amanhan eu terminar

--- agendador-horarios/src/main/java/com/Luciano/agendador_horarios/security/SecurityConfig.java
+++ agendador-horarios/src/main/java/com/Luciano/agendador_horarios/security/SecurityConfig.java
@@ -3,54 +3,109 @@ package com.Luciano.agendador_horarios.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        HttpSecurity httpSecurity = http
+    SecurityFilterChain
+    springSecurityFilterChainTest(org.springframework.security.config.annotation.web.builders.
+                                          HttpSecurity http) throws Exception {
+        http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(request -> {
-                    var config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200"));
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-                    config.setAllowedHeaders(List.of("*"));
-                    config.setAllowCredentials(true);
-                    return config;
-                }))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(basic -> {
-                })
-
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers("/h2-console/**").permitAll()
 
-                        .requestMatchers("/proprietarios/**",
+                        .requestMatchers(HttpMethod.GET, "/login").authenticated()
+
+                        .requestMatchers(
+                                "/proprietarios/**",
                                 "/barbearias/**",
-                                "/servicos/**")
+                                "/servicos/**"
+                        )
                         .hasRole("PROPRIETARIO")
 
                         .requestMatchers("/funcionarios/**")
-                        .hasAnyRole("PROPRIETARIO", "FUNCIONARIO")
+                        .hasAnyRole("PROPRIETARIO",
+                                "FUNCIONARIO")
 
                         .requestMatchers("/agendamentos/**",
                                 "/clientes/**")
                         .authenticated()
 
-                        .anyRequest().denyAll()
+                        .anyRequest().authenticated()
+
                 )
-                        .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+                .httpBasic(withDefaults());
+
+        http.headers(headers ->
+                headers.
+                        frameOptions(
+                                frame -> frame.disable()
+                        ));
+
 
         return http.build();
+
+    }
+
+
+    @Bean
+    UserDetailsService userDetailsServices(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("proprietarioSistema@")
+                .password(encoder.encode("26"))
+                .roles("ROLE_PROPRIETARIO")
+                .build();
+
+        UserDetails solicitante = User.withUsername("funcionarioluc")
+                .password(encoder.encode("2026"))
+                .roles("ROLE_FUNCIONARIO")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, solicitante);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return (CorsConfigurationSource) source;
     }
 }
+
