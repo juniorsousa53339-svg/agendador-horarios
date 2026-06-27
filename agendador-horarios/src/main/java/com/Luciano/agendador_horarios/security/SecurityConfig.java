bb975a1827a08737579469d83c2ce564bdd90d7b
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-27 19:27:32 -0300

    resando

--- agendador-horarios/src/main/java/com/Luciano/agendador_horarios/security/SecurityConfig.java
+++ agendador-horarios/src/main/java/com/Luciano/agendador_horarios/security/SecurityConfig.java
@@ -105,7 +105,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://juniorsousa53339-svg.github.io"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
